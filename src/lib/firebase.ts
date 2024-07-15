5c39f2a7dfb95af1e21c7fa4f71fcd40839ff884
kaito higo <2210391@ecc.ac.jp>
2024-07-15 11:58:51 +0900

    firebaseの管理方法変更

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getAuth,GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyApsM3RVHbsYlBR_NdkMv1QXgTyAoYK_xQ",
-  authDomain: "eploy-2e770.firebaseapp.com",
-  projectId: "eploy-2e770",
-  storageBucket: "eploy-2e770.appspot.com",
-  messagingSenderId: "788316711985",
-  appId: "1:788316711985:web:9973f04a393d5379f0b910"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
