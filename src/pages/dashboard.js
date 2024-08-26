9c6cf4e69102ab83aa321fadfa36f94a709821bd
kouxi08 <kouxi14@gmail.com>
2024-08-26 14:44:23 +0900

    add:goにリクエストできるようにした

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -8,16 +8,63 @@ const Dashboard = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [applications, setApplications] = useState([]);
     const [filteredApplications, setFilteredApplications] = useState([]);
+    const [appStatus, setAppStatus] = useState({});
     const router = useRouter();
 
     useEffect(() => {
-        const fetchApplications = async () => {
+        const updateAppStatus = async () => {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                throw new Error('No token found');
+            }
             try {
-                const token = localStorage.getItem('token');
-                if (!token) {
-                    throw new Error('No token found');
-                }
+                const statusUpdates = await Promise.all(
+                    filteredApplications.map(async (app) => {
+                        const deployment = app.deployment_name;
+                        const response = await fetch(
+                            `/api/app-status/${deployment}`,
+                            {
+                                headers: {
+                                    Authorization: `Bearer ${token}`,
+                                },
+                            },
+                        );
 
+                        if (!response.ok) {
+                            throw new Error(
+                                `Error fetching data: ${response.statusText}`,
+                            );
+                        }
+                        const data = await response.json();
+                        return { id: app.id, status: data.status };
+                    }),
+                );
+
+                const statusMap = statusUpdates.reduce(
+                    (acc, { id, status }) => {
+                        acc[id] = status;
+                        return acc;
+                    },
+                    {},
+                );
+
+                setAppStatus((prevStatus) => ({ ...prevStatus, ...statusMap }));
+            } catch (error) {
+                console.error('Error fetching applications:', error);
+            }
+        };
+        const intervalId = setInterval(updateAppStatus, 20000);
+
+        return () => clearInterval(intervalId);
+    }, [filteredApplications]);
+
+    useEffect(() => {
+        const fetchApplications = async () => {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                throw new Error('No token found');
+            }
+            try {
                 const response = await fetch('/api/dashboardapi', {
                     headers: {
                         Authorization: `Bearer ${token}`,
@@ -84,7 +131,7 @@ const Dashboard = () => {
                                 id={app.id} // assuming each application has a unique id
                                 name={app.name}
                                 url={app.domain}
-                                status={app.status}
+                                status={appStatus[app.id] || app.status}
                                 link={app.domain}
                                 github={app.git_repo_url}
                             />
